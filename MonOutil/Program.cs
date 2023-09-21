578c8d8cd18817869b20039e7e37ee19e7cd9155
felixsoum <felix@felixsoum.com>
2023-09-21 11:30:04 -0400

    Traduction anglaise

--- MonOutil/Program.cs
+++ MonOutil/Program.cs
@@ -2,17 +2,17 @@
 
 // 1) demander le nom de l'utilisateur et l'accueilir
 // ex: bienvenue ___ à l'application MonOutil
-Console.WriteLine("Quel est votre nom?");
+Console.WriteLine("What is your name");
 
 // string interpolation, interpolation de chaine
 string line = Console.ReadLine();
-Console.WriteLine($"Bienvenue {line} à l'application!!");
+Console.WriteLine($"Welcome {line} to the application!!");
 
 // 2) demander l'utilisateur d'entrer une température en Celsius
-Console.WriteLine($"Entrez une température en Celsius!");
+Console.WriteLine($"Enter your temperature in Celsius!");
 double temperatureEnCelcius = Convert.ToDouble(Console.ReadLine());
 double temperatureEnFahrenheit = temperatureEnCelcius * 9 / 5 + 32;
 
 // convertir la température en Fahrenheit et mettre le résultat
 // dans la console
-Console.WriteLine($"Votre température en Fahrenheit est {temperatureEnFahrenheit}");
+Console.WriteLine($"Your temperature in Fahrenheit is {temperatureEnFahrenheit}");
